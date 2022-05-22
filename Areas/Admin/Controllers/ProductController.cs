9016246d4af589c03847bbf078cd4afeb94bc598
honqphonq <lehongphong1103@gmail.com>
2022-05-22 15:45:50 +0700

    done admin

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -27,6 +27,17 @@ namespace OnlineShop.Areas.Admin.Controllers
             return View(_context.Products.Include(c => c.ProductTypes).Include(f => f.SpecialTag).ToList());
         }
 
+        [HttpPost]
+        public IActionResult Index(decimal? lowAmount, decimal? largeAmount)
+        {
+            var products = _context.Products.Include(c => c.ProductTypes).Include(f => f.SpecialTag)
+                .Where(c=>c.Price>=lowAmount && c.Price<=largeAmount).ToList();
+            if(lowAmount == null || largeAmount == null)
+            {
+                products = _context.Products.Include(c => c.ProductTypes).Include(f => f.SpecialTag).ToList();
+            }
+            return View(products);
+        }
         public IActionResult Create()
         {
             ViewData["productTypeId"] = new SelectList(_context.ProductTypes.ToList(), "Id", "ProductType");
@@ -39,6 +50,14 @@ namespace OnlineShop.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                var searchProduct = _context.Products.FirstOrDefault(c => c.Name == products.Name);
+                if(searchProduct != null)
+                {
+                    ViewBag.message = "This product is already exist";
+                    ViewData["productTypeId"] = new SelectList(_context.ProductTypes.ToList(), "Id", "ProductType");
+                    ViewData["TagId"] = new SelectList(_context.SpecialTags.ToList(), "Id", "Name");
+                    return View(products);
+                }
                 if (image != null)
                 {
                     var name = Path.Combine(_hosting.WebRootPath + "/Images", Path.GetFileName(image.FileName));
@@ -51,7 +70,7 @@ namespace OnlineShop.Areas.Admin.Controllers
                 }
                 _context.Products.Add(products);
                 await _context.SaveChangesAsync();
-                TempData["save"] = "Product type has been saved";
+                TempData["save"] = "Product has been saved";
                 return RedirectToAction("Index");
             }
             return View(products);
@@ -83,6 +102,7 @@ namespace OnlineShop.Areas.Admin.Controllers
                 {
                     var name = Path.Combine(_hosting.WebRootPath + "/Images", Path.GetFileName(image.FileName));
                     await image.CopyToAsync(new FileStream(name, FileMode.OpenOrCreate));
+                    TempData["edit"] = "Product has been edited";
                     products.Image = "Images/" + image.FileName;
                 }
 
@@ -100,17 +120,53 @@ namespace OnlineShop.Areas.Admin.Controllers
 
         public ActionResult Details(int id)
         {
-            if(id == null)
+            if (id == null)
             {
                 return NotFound();
             }
-            var product = _context.Products.Include(p => p.ProductTypes).Include(p => p.SpecialTag).FirstOrDefault(p=>p.Id == id);
-            if(product == null)
+            var product = _context.Products.Include(p => p.ProductTypes).Include(p => p.SpecialTag).FirstOrDefault(p => p.Id == id);
+            if (product == null)
             {
                 return NotFound();
             }
             return View(product);
         }
+
+        public ActionResult Delete(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var product = _context.Products.Include(p => p.ProductTypes).Include(p => p.SpecialTag).FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = _context.Products.FirstOrDefault(c => c.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            TempData["delete"] = "Product has been deleted";
+            return RedirectToAction(nameof(Index));
+        }
+
 
     }
 }
